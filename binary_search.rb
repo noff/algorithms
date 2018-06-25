74dc1700c00a53f5d02df71a8f806b83cdc13f7e
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-06-25 17:33:55 +0300

    Binary search recursive

--- binary_search.rb
+++ binary_search.rb
@@ -1,9 +1,27 @@
+# Recursive binary search implementation
+# @param collection [Array]
+# @param item [Any]
+# @return Integer
+def binary_search_recursive(collection, item, from = 0, to = nil)
+	to = (collection.count - 1) if to.nil?
+	middle = (from + to) / 2
+  if item < collection[middle]
+      return binary_search_recursive collection, item, from, middle - 1
+  elsif item > collection[middle]
+      return binary_search_recursive collection, item, middle + 1, to
+  else
+      return middle
+  end
+end
+
 collection = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
-puts find_position('f', collection)
+puts "Binary 'i': #{binary_search_recursive(collection, 'i')}"
+puts "Binary 'e': #{binary_search_recursive(collection, 'e')}"
+puts "Binary 'c': #{binary_search_recursive(collection, 'c')}"
+puts "Binary 'f': #{binary_search_recursive(collection, 'f')}"
+puts "Binary 'e': #{binary_search_recursive(collection, 'e')}"
 
-def find_position(letter, collection)
-	len = collection.length
-	while len >= 1
-	end
-end
+puts '---'
+puts collection.each_with_index.map{|k, i| i}.join(', ')
+puts collection.join(', ')
