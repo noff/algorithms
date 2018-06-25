a6f879108acafb22264c2720678d51825cf5a281
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-06-25 14:21:02 +0300

    first commit

---
+++ binary_search.rb
@@ -0,0 +1,9 @@
+collection = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+puts find_position('f', collection)
+
+def find_position(letter, collection)
+	len = collection.length
+	while len >= 1
+	end
+end
